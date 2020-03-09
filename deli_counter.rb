246f9cda1a95e966cf6fd29067860b499a02665b
Tabassum Hassan <tiram819@gmail.com>
2020-03-09 03:36:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(katz_deli)
+  line_array = []
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else
+    katz_deli.each.with_index(1) do |name, index|
+       line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
+
+
+    
+  
+    
+  
