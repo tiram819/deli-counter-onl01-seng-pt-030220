86b55543e31ecf0c028e77f6ff8e47adeea915bb
Tabassum Hassan <tiram819@gmail.com>
2020-03-09 03:39:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,10 @@ def line(katz_deli)
   end
 end
 
-
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
 
     
   
